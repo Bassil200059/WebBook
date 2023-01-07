8c78b9b795464552e08f5e48c97addf2e2ae3b16
Bassi <Bassi@MSI>
2023-01-08 00:58:59 +0300

    Add project files.

---
+++ Domin/Models/LogBook.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Models
+{
+    public class LogBook
+    {
+        public Guid Id { get; set; }
+        public string  Action { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserId { get; set; }
+
+
+        public Guid BookId  { get; set; }
+        [ForeignKey("BookId")]
+        public virtual Book? Book { get; set; }
+
+    }
+}
---
+++ Domin/Models/LogCategory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domin.Models
+{
+    public class LogCategory
+    {
+        public Guid Id { get; set; }
+        [Required]
+        public string Action { get; set; }
+        public DateTime Date { get; set; }
+        public Guid UserId { get; set; }
+
+
+
+        public Guid CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public virtual Category? Category { get; set; }
+
+    }
+}
